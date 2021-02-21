55df6de9ea80dbc713a6f64c475e21cea3f290f7
KP191458 <191458@edu.p.lodz.pl>
2021-02-21 12:59:48 +0100

    Beginnning of thesis part, simple Arduino sheets for Lidar reading and simple python script for plots.

--- Misc/graph2.py
+++ Misc/graph2.py
@@ -4,8 +4,6 @@ import pyqtgraph as pg
 import sys  # We need sys so that we can pass argv to QApplication
 import os
 from random import randint
-import serial
-ser = serial.Serial("COM3", 9600)
 
 class MainWindow(QtWidgets.QMainWindow):
 
@@ -15,8 +13,8 @@ class MainWindow(QtWidgets.QMainWindow):
         self.graphWidget = pg.PlotWidget()
         self.setCentralWidget(self.graphWidget)
 
-        self.x = list(range(400))  # 100 time points
-        self.y = [randint(0,100) for _ in range(400)]  # 100 data points
+        self.x = list(range(100))  # 100 time points
+        self.y = [randint(0,100) for _ in range(100)]  # 100 data points
 
         self.graphWidget.setBackground('w')
 
@@ -28,13 +26,12 @@ class MainWindow(QtWidgets.QMainWindow):
         self.timer.start()
 
     def update_plot_data(self):
-        value = int(ser.readline(10).decode('utf-8'))
-        
+
         self.x = self.x[1:]  # Remove the first y element.
         self.x.append(self.x[-1] + 1)  # Add a new value 1 higher than the last.
 
         self.y = self.y[1:]  # Remove the first 
-        self.y.append(value)  # Add a new random value.
+        self.y.append( randint(0,100))  # Add a new random value.
 
         self.data_line.setData(self.x, self.y)  # Update the data.
 
